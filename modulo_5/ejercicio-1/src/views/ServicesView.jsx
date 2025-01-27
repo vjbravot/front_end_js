242238e0c2d640dd7b54aa73a0daa7dbc8c10b30
vjbravot <vicente.bravo@driv.in>
2025-01-26 22:49:48 -0300

    fixes in api request

--- modulo_5/ejercicio-1/src/views/ServicesView.jsx
+++ modulo_5/ejercicio-1/src/views/ServicesView.jsx
@@ -1,33 +1,18 @@
-import { ViewProvider } from "../context/ViewContext.jsx";
 import ServicesList from "../components/ServicesList.jsx";
+import MainLayout from "../layouts/MainLayout.jsx"
+import { useAuth } from "../context/AuthContext";
+import { ServicesProvider } from "../context/ServicesContext.jsx"
+function Services() {
+  const { user } = useAuth();
 
-function Services(){
-    useEffect(() => {
-        async function callServices() {
-          try {
-            console.log("calling Drs");
-            let response = await fetch("/services.json");
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            let data = await response.json();
-            setServices(data);
-            console.log("Data loaded successfully: ", data)
-          } catch (error) {
-            console.error("Error fetching doctors:", error);
-            setServices([]); // Fallback to an empty array
-          }
-        }
-      callServices();
-        }, []);
-
-    return (
-        <ContextProvider>
-            <div>
-                <ServicesList />
-            </div>
-        </ContextProvider>
-    )
+  return (
+    <ServicesProvider>
+      <div>
+        <MainLayout />
+        <ServicesList />
+      </div>
+    </ServicesProvider>
+  )
 }
 
 export default Services;
